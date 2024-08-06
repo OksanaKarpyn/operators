ca8a6cc2b84cbe17d3b5382aacbbf4910565d3a7
Oksana.Karpyn <oksana101992@gmail.com>
2024-08-06 16:58:25 +0200

    cerate login component

---
+++ src/app/pages/login-operator/login-operator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-operator',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-operator.component.html',
+  styleUrl: './login-operator.component.scss'
+})
+export class LoginOperatorComponent {
+
+}
