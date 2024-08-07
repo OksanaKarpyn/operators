a3fb575ba5b4ae6d518ac713a3f6a21e64e368fe
Oksana.Karpyn <oksana101992@gmail.com>
2024-08-07 14:38:21 +0200

    edit  login component

--- src/app/pages/login-operator/login-operator.component.ts
+++ src/app/pages/login-operator/login-operator.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { OperatorsService } from '../../services/operators.service';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, UntypedFormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login-operator',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe,
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './login-operator.component.html',
-  styleUrl: './login-operator.component.scss'
+  styleUrls: ['./login-operator.component.scss']
 })
 export class LoginOperatorComponent {
+  loginForm!: UntypedFormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private operatorsService: OperatorsService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  submit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.operatorsService.login(email, password).subscribe(success => {
+        if (success) {
+          const operator = this.operatorsService.getCurrentOperator();
+          if (operator) {
+            // Operatore trovato e autenticato, redirigi alla dashboard personale
+            this.router.navigate([`/dashboard/${operator.id}`]);
+          } else {
+            // Questo caso non dovrebbe accadere, ma è meglio gestirlo
+            alert('Si è verificato un errore durante il login. Riprovare.');
+          }
+        } else {
+          // Operatore non trovato o credenziali errate
+          alert('Email o password errati');
+        }
+      });
+    }
+  }
 }
