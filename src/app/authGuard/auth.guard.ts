03a03d45aa3a8d70058c6aa7f613c18731904900
Oksana.Karpyn <oksana101992@gmail.com>
2024-08-07 14:38:49 +0200

    add file augth

---
+++ src/app/authGuard/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { OperatorsService } from '../services/operators.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private operatorsService: OperatorsService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.operatorsService.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
