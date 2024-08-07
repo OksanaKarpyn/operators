a0d2955040b4a9d7d28db41c3e54135c8dab8bb2
Oksana.Karpyn <oksana101992@gmail.com>
2024-08-07 14:39:31 +0200

    edit dashboard component

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component} from '@angular/core';
+import { Operator } from '../../models/operator';
+import { JsonPipe } from '@angular/common';
+import { ActivatedRoute, RouterLink ,Router} from '@angular/router';
+import { OperatorsService } from '../../services/operators.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [JsonPipe,RouterLink],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  
+  operator: Operator | null = null; 
+
+  constructor(private route: ActivatedRoute,private router: Router , private operatorsService: OperatorsService) {}
+
+  ngOnInit(): void {
+    const operatorId = this.route.snapshot.paramMap.get('id');
+    if (operatorId) {
+      this.operatorsService.getOperatorById(operatorId).subscribe(operator => {
+        this.operator = operator;
+      });
+    }
+  }
+
+  logout(): void {
+    this.operatorsService.logout();
+    this.router.navigate(['/login']);
+  }
+
+}
