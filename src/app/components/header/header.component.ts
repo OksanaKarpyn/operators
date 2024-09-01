bf77499ba7c8319457978f5ae14ae706a714f405
Oksana-Karpyn <oksana.karpyn@anitia.it>
2024-09-01 11:18:18 +0200

    remuve auth

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { JsonPipe,CommonModule } from '@angular/common';
+import { JsonPipe, CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { Subscription } from 'rxjs';
@@ -13,17 +13,15 @@ import { User } from '../../models/user';
     JsonPipe,
     RouterLink,
     CommonModule,
-   
+
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit,OnDestroy {
-  isAuthenticated = false;
-  userId?: string | null = null;
-  sub?: Subscription;
-  user?: User |undefined;
-   
+export class HeaderComponent implements OnInit {
+
+  user?: User | undefined;
+
   canViewRegisterButton = false;
   canViewEditButton = false;
   canViewDeleteButton = false;
@@ -31,40 +29,23 @@ export class HeaderComponent implements OnInit,OnDestroy {
 
   isNavbarShown = document.querySelector('.navbar-collapse.show');
   constructor(
-    private authService:AuthService,
-    private userService:UserService,
+    private authService: AuthService,
+    private userService: UserService,
     private router: Router) {
-      userService.profile$.subscribe({
-        
-        next:(user)=>{
-          this.user = user;
-          this.updateButtonVisibility(); // Aggiorna la visibilità dei pulsanti
-        }
-      })
-  }
-  
-  
-  ngOnInit():void{
-    this.isAuthenticated = this.userService.isAuthenticated();
-    
+    userService.profile$.subscribe({
 
-    this.sub = this.userService.isAuthenticated$.subscribe((value) => {
-
-      this.isAuthenticated = value;
-
-      if (value) {
-        this.userService.getCurrentUser().subscribe(user => {
-          if (user) {
-            console.log(user.id,'header component');
-            this.userId= user.id// Memorizza l'ID dell'operatore loggato
-            this.updateButtonVisibility(); // Aggiorna la visibilità dei pulsanti
-          }
-        });
+      next: (user) => {
+        this.user = user;
+        this.updateButtonVisibility(); // Aggiorna la visibilità dei pulsanti
       }
     })
-   
   }
-//closenavBar
+
+
+  ngOnInit(): void {
+
+  }
+  //closenavBar
   closeNavBar() {
     const navbarCollapse = document.querySelector('#navbarNav');
     if (navbarCollapse?.classList.contains('show')) {
@@ -73,22 +54,16 @@ export class HeaderComponent implements OnInit,OnDestroy {
   }
 
 
-  updateButtonVisibility():void {
-    if(this.user){
+  updateButtonVisibility(): void {
+    if (this.user) {
       const visibility = this.userService.getButtonVisibility(this.user.role);
       this.canViewRegisterButton = visibility.canViewRegisterButton;
       this.canViewEditButton = visibility.canViewEditButton;
       this.canViewDeleteButton = visibility.canViewDeleteButton;
     }
-   }
+  }
+
   logout(): void {
     this.authService.logout();
   }
-
- ngOnDestroy(): void {
-   this.sub?.unsubscribe()
- }
-
-
-
 }
