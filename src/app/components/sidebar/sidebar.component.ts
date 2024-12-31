363871c3dbc783a2e64873ef578dc440aa9b8257
Oksana-Karpyn <oksana.karpyn@anitia.it>
2024-12-31 10:26:25 +0100

    remove space in different file

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { JsonPipe,CommonModule } from '@angular/common';
+import { JsonPipe, CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
@@ -22,8 +22,8 @@ import { RolePipe } from '../../pipes/role.pipe';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent implements OnInit {
-  
-  user?: User |undefined;
+
+  user?: User | undefined;
   isDarkTheme = false;
   //---role---
   canRegister = false;
@@ -31,49 +31,47 @@ export class SidebarComponent implements OnInit {
   canView = false;
 
   constructor(
-    private authService:AuthService,
-    private userService:UserService,
-    private themeService :ThemeService
-  ){
+    private authService: AuthService,
+    private userService: UserService,
+    private themeService: ThemeService
+  ) {
 
     userService.profile$.subscribe({
-        
-      next:(user)=>{
-        this.user = user;
-        if(user){
 
-          this.canRegister =  this.userService.hasRole1(['admin'])
-          this.canEdit = this.userService.hasRole1(['admin','operator'])
+      next: (user) => {
+        this.user = user;
+        if (user) {
+
+          this.canRegister = this.userService.hasRole1(['admin'])
+          this.canEdit = this.userService.hasRole1(['admin', 'operator'])
         }
       }
     })
   }
 
-  ngOnInit():void{     
+  ngOnInit(): void {
     //-------role------
 
-  //   this.userService.hasRole(['admin']).subscribe({
-  //     next:(admin)=>{
-  //       this.isAdmin= admin;
-  //     },
-  //     error:(err)=>{
-  //       console.error(err);
-  //       this.canEdit= false;
-  //     }
-  //   });
-   
-  // this.userService.hasRole(['admin','operator']).subscribe({
-  //   next:(canEdit)=>{
-  //     this.canEdit= canEdit;
-  //   },
-  //   error:(err)=>{
-  //     console.error(err);
-  //     this.canEdit= false;
-  //   }
-  // });
-
-  // this.canEdit = this.userService.hasRole1(['admin','operator'])
-   //this.isAdmin = this.userService.hasRole1(['admin'])
+    //   this.userService.hasRole(['admin']).subscribe({
+    //     next:(admin)=>{
+    //       this.isAdmin= admin;
+    //     },
+    //     error:(err)=>{
+    //       console.error(err);
+    //       this.canEdit= false;
+    //     }
+    //   });
+    // this.userService.hasRole(['admin','operator']).subscribe({
+    //   next:(canEdit)=>{
+    //     this.canEdit= canEdit;
+    //   },
+    //   error:(err)=>{
+    //     console.error(err);
+    //     this.canEdit= false;
+    //   }
+    // });
+    // this.canEdit = this.userService.hasRole1(['admin','operator'])
+    //this.isAdmin = this.userService.hasRole1(['admin'])
 
     //--------theme---------
     this.themeService.theme$.subscribe(theme => {
@@ -81,12 +79,12 @@ export class SidebarComponent implements OnInit {
     });
   }
   //------function-theme------
-  toggleTheme(){
+  toggleTheme() {
     const newTheme = this.isDarkTheme ? 'light-theme' : 'dark-theme';
     this.themeService.setTheme(newTheme);
   }
 
-   logout(): void {
+  logout(): void {
     this.authService.logout();
   }
 
